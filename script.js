d1a53951e6fc4a7d8dfd9c502d17f1e32be76f78
Steeve MOKOKO <steeve.mokoko14@gmail.com>
2025-02-07 14:41:18 +0100

    Correction bug scroll vertical sur petits écrans avec un dvh

---
+++ script.js
@@ -0,0 +1,10 @@
+// Pas encore de code JavaScript ici
+
+// Vous êtes développeur ? Envoyez nous une candidature spontanée en mentionnant le terme "BBOXA"
+// Email : info@qatorze.com
+
+// Postes à pourvoir : 
+// - Développeur Front-End Angular
+// - Développeur Full Stack Java & Angular
+// - Développeur Back-End Java - Expérimenté
+// - Développeur Mobile Android (Java et Kotlin) 
